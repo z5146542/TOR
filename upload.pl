fc5525d0df84051976b5437cbcd50baba42c0c69
Seung Hoon Park <z5146542@student.unsw.edu.au>
2018-12-03 16:40:19 +1100

    worked at Mon  3 Dec 16:40:19 AEDT 2018

---
+++ upload.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/perl -w
+
+use strict;
+use warnings;
+
+my $date = `date`;
+chomp $date;
+
+print `git status` and  print `git add .` and print `git status` and print `git commit -m "worked at $date"` and print `git status` and print `git push` and print `git status`;
